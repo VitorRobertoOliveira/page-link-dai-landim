58ac6ad990ea01acead8b1d4e83f340d66d6f7d3
VitorRobertoOliveira <vitorrobertooliveira@gmail.com>
2025-03-04 23:36:10 -0300

    wip

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,29 +19,23 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
-document.getElementById('agendar-btn').addEventListener('click', function(event) {
-    event.preventDefault(); // Impede o comportamento padrão do link
+document.getElementById('agendar-btn').addEventListener('click', function (event) {
+    event.preventDefault();
 
-    // Captura os valores dos campos do formulário
     const nomeInput = document.getElementById('nome');
     const servicoInput = document.getElementById('select');
 
     const nome = nomeInput.value;
     const servico = servicoInput.value;
 
-    // Constrói a mensagem
     const mensagem = `Olá, meu nome é ${nome}.\nGostaria de agendar um serviço de ${servico}.\nQuais são os horários disponíveis ?`;
 
-    // Constrói a URL do WhatsApp
     const url = `https://api.whatsapp.com/send?phone=5534984401206&text=${encodeURIComponent(mensagem)}`;
 
-    // Redireciona o usuário para o WhatsApp
     window.open(url, '_blank');
 
-    // Limpa os campos do formulário
     nomeInput.value = "";
     servicoInput.value = "";
 
-    // Opcional: Fechar o modal se necessário
     document.getElementById("modalForm").style.display = "none";
 });
